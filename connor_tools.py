ca4e2bc2e6886d7cbebb17286d1cbbf67411f5dd
Thomas Connor <connort1@msu.edu>
2014-11-24 18:38:48 -0500

    Creating repo

---
+++ connor_tools.py
@@ -0,0 +1,41 @@
+import numpy as np
+from matplotlib import pyplot as pt
+
+def great_circle(dec1,dec2,ra1,ra2,decradians=True,raradians=True,out='arcsec',haversine=False):
+    '''Computes great circle angle between two coordinates in RA and Dec
+    Inputs:
+    dec1,ra1 -- position of object 1 (float)
+    dec2,ra2 -- position of object2 (float)
+    decradians -- are declinations in radians (default=True)
+    raradians -- are right ascenscions in radians (default=True)
+    out -- Outout format. Options: 'arcsec' (default), 'arcmin', 'degree'
+    haversine -- Use haversine approximation (computationally more accurate for small angles) (default=False)'''
+    if not decradians:
+        dec1 = np.radians(dec1)
+        dec2 = np.radians(dec2)
+    if not raradians:
+        ra1 = np.radians(15.*ra1)
+        ra2 = np.radians(15.*ra2)
+    del_ra = ra1 - ra2
+    if haversine:
+        del_dec = dec1 - dec2
+        term1 = np.sin(del_dec/2.)**2.
+        term2 = np.cos(dec1) * np.cos(dec2)
+        term3 = np.sin(del_ra/2.)**2.
+        sigma_angle =  2.* np.arcsin(np.sqrt(term1 + term2*term3))
+    else:
+        term1 = np.sin(dec1) * np.sin(dec2)
+        term2 = np.cos(dec1) * np.cos(dec2)
+        term3 = np.cos(del_ra)
+        sigma_angle = np.arccos(term1 + term2*term3)
+    sigma_angle = np.degrees(sigma_angle)
+    if out=='degree':
+        return sigma_angle
+    elif out=='arcmin':
+        return sigma_angle * 60.
+    elif out=='arcsec':
+        return sigma_angle * 3600.
+    else:
+        print 'Invalid Output format for Great Circle Distance; returning arcseconds'
+        return sigma_angle * 3600.
+    
